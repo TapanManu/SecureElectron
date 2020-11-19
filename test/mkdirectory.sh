a7fa0f3b5253c5721293a5ee2afed584d541990d
TapanManu <tapanmanu2000@gmail.com>
2020-11-19 21:36:33 +0530

    Revert "initial commit"
    This reverts commit 711d53839c4dec51808b22778fe38e82ab14f46a.

--- test/mkdirectory.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-mkdir testdir
-
-for((i=0;i<10;i++))
-do
-    truncate -s 1M testdir/file$i.txt
-done
-
--- test/preload.js
+++
@@ -1,6 +0,0 @@
-const { readFileSync } = require('fs')
-
-window.readConfig = function () {
-  const data = readFileSync('./config.json')
-  return data
-}
