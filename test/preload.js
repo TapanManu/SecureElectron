7f6ebde28fdd79619ae68e57727005ccd802aaad
TapanManu <tapanmanu2000@gmail.com>
2020-12-01 21:08:30 +0530

    added license

--- test/preload.js
+++ test/preload.js
@@ -1,4 +1,20 @@
-const { readFileSync } = require("fs");
+
+/* 
+ * This file is part of the  distribution (https://github.com/aicore/SecureElectron).
+ * Copyright (c) 2015 <your name> and Core.ai 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */const { readFileSync } = require("fs");
 
 window.readConfig = function () {
   const data = readFileSync("./config.json");
--- test/rddir.js
+++ test/rddir.js
@@ -1,3 +1,19 @@
+/* 
+ * This file is part of the  distribution (https://github.com/aicore/SecureElectron).
+ * Copyright (c) 2015 <your name> and Core.ai 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
 const { app, BrowserWindow, dialog, remote } = require("electron");
 const path = require("path");
 
